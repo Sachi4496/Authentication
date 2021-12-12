f14e4d074f1cbf53cc827ff0bbf179762c2d0152
Sachidanand Pradhan <sachidanand.kpradhan@gmail.com>
2021-12-12 18:09:53 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const { register , login } = require('./controllers/auth.controller');
 
+const productController = require('./controllers/product.controller');
+
 const app = express();
 
 app.use(express.json());
 
 app.post("/register", register);
 app.post("/login", login)
+
+app.use("/products", productController);
 
 module.exports = app;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
-const { Sechma, model } = require("mongoose");
+const bcrypt = require('bcryptjs');
+
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
     {
@@ -10,5 +12,29 @@ const userSchema = new Schema(
         timestamps: true,
     }
 );
+
+userSchema.pre("save", function(next) { 
+    // create and update
+    if (!this.isModified("password")) return next();
+    bcrypt.hash(this.password, 10, (err, hash) => {
+        this.password = hash;
+        return next();
+    });
+    //or
+    // if(!this.isModified("password")) return next();
+    // const hash = bcrypt.hashSync("bacon", 10);
+    // this.password = hash;
+    // return next();
+});
+
+userSchema.methods.checkPassword = function(password) {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, this.password, function(err, same) {
+            if(err) return reject(err);
+    
+            return resolve(same);
+        });
+    });
+}
 
 module.exports = model("user", userSchema); // users
